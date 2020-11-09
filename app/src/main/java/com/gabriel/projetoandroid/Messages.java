bc5f33d3e711b02526804bda84f5d12c2522fd01
Cassio-Marciao <64213641+Cassio-Marciao@users.noreply.github.com>
2020-11-08 20:42:31 -0400

    Chat Funcionando

---
+++ app/src/main/java/com/gabriel/projetoandroid/Messages.java
@@ -0,0 +1,39 @@
+package com.gabriel.projetoandroid.messages;
+
+public class Messages {
+
+    private String text, fromId, toId;
+    private long timestamp;
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(long timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public String getFromId() {
+        return fromId;
+    }
+
+    public void setFromId(String fromId) {
+        this.fromId = fromId;
+    }
+
+    public String getToId() {
+        return toId;
+    }
+
+    public void setToId(String toId) {
+        this.toId = toId;
+    }
+}
--- app/src/main/java/com/gabriel/projetoandroid/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/com/gabriel/projetoandroid/ui/notifications/NotificationsFragment.java
@@ -1,9 +1,11 @@
-package com.gabriel.projetoandroid.ui.notifications;
+package com.gabriel.projetoandroid;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -11,15 +13,53 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.gabriel.projetoandroid.R;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentChange;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.xwray.groupie.GroupAdapter;
+
+import java.util.List;
 
 public class NotificationsFragment extends Fragment {
 
+    private EditText txtMessageChat;
+    private FloatingActionButton btnSendMessage;
+    private Usuario me;
+    private GroupAdapter adapter;
+
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-
         View root = inflater.inflate(R.layout.fragment_notifications, container, false);
+
+        txtMessageChat = root.findViewById(R.id.txtMessageChat);
+        btnSendMessage = root.findViewById(R.id.btnSendChat);
+        RecyclerView rcView = root.findViewById(R.id.rcView);
+
+        adapter = new GroupAdapter();
+        rcView.setLayoutManager(new LinearLayoutManager(getContext()));
+        rcView.setAdapter(adapter);
+
+        FirebaseFirestore.getInstance().collection("/usuarios")
+                .document(FirebaseAuth.getInstance().getUid())
+                .get()
+                .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                    @Override
+                    public void onSuccess(DocumentSnapshot documentSnapshot) {
+                        Usuario a = documentSnapshot.toObject(Usuario.class);
+                        fetchMessages();
+                    }
+                });
 
         return root;
     }
