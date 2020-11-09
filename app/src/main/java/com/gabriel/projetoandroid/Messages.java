deeae52cdccfa8c14a7340cadc47ee10427e9192
Gabriel Freire <gabrielfreiredesouza.souza@gmail.com>
2020-11-08 20:00:56 -0700

    projeto final com chat

--- app/src/main/java/com/gabriel/projetoandroid/Messages.java
+++ app/src/main/java/com/gabriel/projetoandroid/Messages.java
@@ -1,4 +1,4 @@
-package com.gabriel.projetoandroid.messages;
+package com.gabriel.projetoandroid;
 
 public class Messages {
 
--- app/src/main/java/com/gabriel/projetoandroid/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/com/gabriel/projetoandroid/ui/notifications/NotificationsFragment.java
@@ -1,35 +1,23 @@
-package com.gabriel.projetoandroid;
+package com.gabriel.projetoandroid.ui.notifications.;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.gabriel.projetoandroid.R;
+import com.gabriel.projetoandroid.Usuario;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.firestore.DocumentChange;
 import com.google.firebase.firestore.DocumentSnapshot;
-import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.FirebaseFirestoreException;
-import com.google.firebase.firestore.Query;
-import com.google.firebase.firestore.QuerySnapshot;
 import com.xwray.groupie.GroupAdapter;
-
-import java.util.List;
 
 public class NotificationsFragment extends Fragment {
 
